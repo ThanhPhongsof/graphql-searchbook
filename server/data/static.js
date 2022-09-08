494028e16f2b4b3fd787866cdcf2422d5fa07994
ThanhPhongsof <ttphongletter@gmail.com>
2022-09-08 21:41:47 +0700

    end p1

--- server/data/static.js
+++ server/data/static.js
@@ -1,13 +1,39 @@
 const books = [
   {
     id: 1,
-    name: "De men phieu luu ky",
-    genre: "Adventure",
+    name: "Tat den",
+    genre: "Tieu thuyet",
+    authorId: 1,
   },
   {
     id: 2,
     name: "Lam giau khong kho",
-    genre: "Education",
+    genre: "Truyen ngan",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "So do",
+    genre: "Tieu thuyet",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "Doi thua",
+    genre: "Truyen ngan",
+    authorId: 2,
+  },
+  {
+    id: 5,
+    name: "Song mon",
+    genre: "Truyen ngan",
+    authorId: 2,
+  },
+  {
+    id: 6,
+    name: "Ky nghe lay tay",
+    genre: "Phong su",
+    authorId: 3,
   },
 ];
 
