06848dfbf03dee91aba071e4d22616840315e962
Phong Tran <ttphongletter@gmail.com>
2022-09-08 17:19:58 +0700

    p1 - 44:53

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+// Load schema & resolvers
+const typeDefs = require("./schema/schema");
+const resolvers = require("./resolver/resolver");
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const app = express();
+
+// server.applyMiddleware({ app });
+
+// app.listen({ port: process.env.PORT || 4000 }, () =>
+//   console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+// );
+
+async function startApolloServer(typeDefs, resolvers) {
+  // Same ApolloServer initialization as before
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  // Required logic for integrating with Express
+  await server.start();
+
+  const app = express();
+
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: "/",
+  });
+
+  // Modified server startup
+  await new Promise((resolve) => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers);
---
+++ server/data/static.js
@@ -0,0 +1,32 @@
+const books = [
+  {
+    id: 1,
+    name: "De men phieu luu ky",
+    genre: "Adventure",
+  },
+  {
+    id: 2,
+    name: "Lam giau khong kho",
+    genre: "Education",
+  },
+];
+
+const authors = [
+  {
+    id: 1,
+    name: "Ngo Tat to",
+    age: 127,
+  },
+  {
+    id: 2,
+    name: "Nam Cao",
+    age: 105,
+  },
+  {
+    id: 3,
+    name: "Vu Trong Phung",
+    age: 109,
+  },
+];
+
+module.exports = { books, authors };
